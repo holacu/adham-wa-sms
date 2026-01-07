a65b5581e7e077a3389e07fb575af9c6ccc61ed5
holacu <53341555+holacu@users.noreply.github.com>
2026-01-07 22:24:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ const client = new Client({
     }),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null
     }
 });
